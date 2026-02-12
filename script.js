f28b2a1a327511a1168f9730a9165b11966a17b4
Mai-Shinbo <maimaimilk63@gmail.com>
2026-02-12 23:42:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // 地図初期化
-const map = L.map("map").setView([33.5597, 133.5311], 13);
+const map = L.map("map").setView([33.5588836,133.5312602], 13);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "© OpenStreetMap"
